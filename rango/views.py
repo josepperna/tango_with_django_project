6ee1ca2f413ec2ee86aaf34cb3b7bedf2b1d55ec
Josep Perna <josepperna@gmail.com>
2019-01-30 21:29:22 +0000

    Chapter 8 finished

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,6 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-
     return render(request, 'rango/index.html', context=context_dict)
 
 
